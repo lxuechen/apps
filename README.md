683159d182d9dfe5a0daeb655ac29b0e083722a1
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2022-02-21 21:20:47 -0800

    Refactor.

--- README.md
+++ README.md
@@ -14,6 +14,11 @@ For other benchmarks of enormous Transformers, see a dataset which tests ability
 
 The training instructions are specified in [train/README](apps/train/README.md) and similarly the evaluation instructions are specified in [eval/README](apps/eval/README.md).
 
+### lxuechen: Using
+- put data in the folder `~/data/apps` (this is also data root).
+- generate paths by running `python train/apps_create_split.py`.
+- from `train/` run `python -m dataset_apps.APPSBaseDataset`.
+
 ## Citation
 
 If you find this useful in your research, please consider citing
--- train/apps_create_split.py
+++ train/apps_create_split.py
@@ -2,21 +2,6 @@ import json
 import os
 import pathlib
 
-
-def create_split(split="train", name="train"):
-    paths = []
-    print(os.path.exists(split))
-    roots = sorted(os.listdir(split))
-    for folder in roots:
-        root_path = os.path.join(split, folder)
-        paths.append(root_path)
-
-    with open(name + ".json", "w") as f:
-        json.dump(paths, f)
-
-    return paths
-
-
 # TODO: insert path to train and test
 #   path should be relative to root directory or absolute paths
 dataroot = "/Users/xuechenli/data/apps/"
@@ -24,10 +9,28 @@ dataroot = "/Users/xuechenli/data/apps/"
 paths_to_probs = [os.path.join(dataroot, "APPS/train"), os.path.join(dataroot, "APPS/test")]
 names = ["train", "test"]
 
+data_split_dir = os.path.join(dataroot, 'data_split')
+os.makedirs(data_split_dir, exist_ok=True)
+
+def create_split(split="train", name="train"):
+    paths = []
+    roots = sorted(os.listdir(split))
+    for folder in roots:
+        root_path = os.path.join(split, folder)
+        paths.append(root_path)
+
+    target_path = os.path.join(data_split_dir, name + ".json")
+    with open(target_path, "w") as f:
+        json.dump(paths, f)
+
+    return paths
+
+
 all_paths = []
 for index in range(len(paths_to_probs)):
     all_paths.extend(create_split(split=paths_to_probs[index], name=names[index]))
 
-with open("train_and_test.json", "w") as f:
+target_path = os.path.join(data_split_dir, "train_and_test.json")
+with open(target_path, "w") as f:
     print(f"Writing all paths. Length = {len(all_paths)}")
     json.dump(all_paths, f)
